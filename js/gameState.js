78eaf90bb6e846c79a2cc774b2e429e3507b81c2
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-04-12 22:32:09 +0300

    Merge conflicts and new function in gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -13,3 +13,33 @@ export function createInitialState() {
     };
 }
 
+/**
+ * Processing a new input value and returning the next state.
+ */
+export function processInput(state, rawVal) {
+    let val = rawVal.slice(0, state.target.length);
+
+    let errors = state.errors;
+    let totalTyped = state.totalTyped;
+
+    // Count new character, only on additions, not deletions
+    if (val.length > state.typed.length) {
+        totalTyped++;
+        if (val[val.length - 1] !== state.target[val.length - 1]) {
+            errors++;
+        }
+    }
+
+    const didFinish = val === state.target;
+
+    return {
+        state: {
+            ...state,
+            typed: val,
+            errors,
+            totalTyped,
+            finished: didFinish,
+        },
+        didFinish,
+    };
+}
