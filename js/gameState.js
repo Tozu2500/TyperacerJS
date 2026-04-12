a55d76450bad3a863a01f295262442df3fa3085d
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-04-12 22:17:38 +0300

    Resolve merge conflict, keep local.

--- js/gameState.js
+++ js/gameState.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Manages all mutable game state and core logic
  */
@@ -14,20 +13,3 @@ export function createInitialState() {
     };
 }
 
-=======
-/**
- * Manages all mutable game state and core logic
- */
-
-export function createInitialState() {
-    return {
-        target: "",
-        typed: "",
-        startTime: null,
-        finished: false,
-        errors: 0,
-        totalTyped: 0,
-    };
-}
-
->>>>>>> 9e8f915b9567b2773ad732628ef1048ae039575e
