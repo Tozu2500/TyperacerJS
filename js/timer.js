703f6cd6b3232bae74d0f32a66de1657fbc06777
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-04-13 00:24:56 +0300

    Timer module/wrapper for timing functionality

--- js/timer.js
+++ js/timer.js
@@ -3,3 +3,16 @@
  */
 
 let _interval = null;
+
+export function startTicker(onTick, intervalMs = 300) {
+    stopTicker();
+    _interval = setInterval(onTick, intervalMs);
+}
+
+// Stop timer
+export function stopTicker() {
+    if (_interval !== null) {
+        clearInterval(_interval);
+        _interval = null;
+    }
+}
